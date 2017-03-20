7e70c99b33215bb362a055925ec0df07f8e14160
Wesley Fantinel <wesley.fantinel@gmail.com>
2017-03-20 15:54:33 -0300

    Adding Elo bin start numbers

--- creditcard.go
+++ creditcard.go
@@ -135,6 +135,24 @@ func (c *Card) MethodValidate() (Company, error) {
 	}
 
 	switch {
+	case ccDigits.At(4) == 4011 || ccDigits.At(6) == 431274 || ccDigits.At(6) == 438935 ||
+		ccDigits.At(6) == 451416 || ccDigits.At(6) == 457393 || ccDigits.At(4) == 4576 ||
+		ccDigits.At(6) == 457631 || ccDigits.At(6) == 457632 || ccDigits.At(6) == 504175 ||
+		ccDigits.At(6) == 627780 || ccDigits.At(6) == 636297 || ccDigits.At(6) == 636368 ||
+		ccDigits.At(6) == 636369 || (ccDigits.At(6) >= 506699 && ccDigits.At(6) <= 506778) ||
+		(ccDigits.At(6) >= 509000 && ccDigits.At(6) <= 509999) ||
+		(ccDigits.At(6) >= 650031 && ccDigits.At(6) <= 650051) ||
+		(ccDigits.At(6) >= 650035 && ccDigits.At(6) <= 650033) ||
+		(ccDigits.At(6) >= 650405 && ccDigits.At(6) <= 650439) ||
+		(ccDigits.At(6) >= 650485 && ccDigits.At(6) <= 650538) ||
+		(ccDigits.At(6) >= 650541 && ccDigits.At(6) <= 650598) ||
+		(ccDigits.At(6) >= 650700 && ccDigits.At(6) <= 650718) ||
+		(ccDigits.At(6) >= 650720 && ccDigits.At(6) <= 650727) ||
+		(ccDigits.At(6) >= 650901 && ccDigits.At(6) <= 650920) ||
+		(ccDigits.At(6) >= 651652 && ccDigits.At(6) <= 651679) ||
+		(ccDigits.At(6) >= 655000 && ccDigits.At(6) <= 655019) ||
+		(ccDigits.At(6) >= 655021 && ccDigits.At(6) <= 655021):
+		return Company{"elo", "Elo"}, nil
 	case ccDigits.At(2) == 34 || ccDigits.At(2) == 37:
 		return Company{"amex", "American Express"}, nil
 	case ccDigits.At(4) == 5610 || (ccDigits.At(6) >= 560221 && ccDigits.At(6) <= 560225):
@@ -145,9 +163,11 @@ func (c *Card) MethodValidate() (Company, error) {
 		return Company{"diners club carte blanche", "Diners Club Carte Blanche"}, nil
 	case ccDigits.At(4) == 2014 || ccDigits.At(4) == 2149:
 		return Company{"diners club enroute", "Diners Club enRoute"}, nil
-	case ((ccDigits.At(3) >= 300 && ccDigits.At(3) <= 305) || ccDigits.At(3) == 309 || ccDigits.At(2) == 36 || ccDigits.At(2) == 38 || ccDigits.At(2) == 39) && ccLen <= 14:
+	case ((ccDigits.At(3) >= 300 && ccDigits.At(3) <= 305) || ccDigits.At(3) == 309 ||
+		ccDigits.At(2) == 36 || ccDigits.At(2) == 38 || ccDigits.At(2) == 39) && ccLen <= 14:
 		return Company{"diners club international", "Diners Club International"}, nil
-	case ccDigits.At(4) == 6011 || (ccDigits.At(6) >= 622126 && ccDigits.At(6) <= 622925) || (ccDigits.At(3) >= 644 && ccDigits.At(3) <= 649) || ccDigits.At(2) == 65:
+	case ccDigits.At(4) == 6011 || (ccDigits.At(6) >= 622126 && ccDigits.At(6) <= 622925) ||
+		(ccDigits.At(3) >= 644 && ccDigits.At(3) <= 649) || ccDigits.At(2) == 65:
 		return Company{"discover", "Discover"}, nil
 	case ccDigits.At(3) == 636 && ccLen >= 16 && ccLen <= 19:
 		return Company{"interpayment", "InterPayment"}, nil
@@ -155,13 +175,17 @@ func (c *Card) MethodValidate() (Company, error) {
 		return Company{"instapayment", "InstaPayment"}, nil
 	case ccDigits.At(4) >= 3528 && ccDigits.At(4) <= 3589:
 		return Company{"jcb", "JCB"}, nil
-	case ccDigits.At(4) == 5018 || ccDigits.At(4) == 5020 || ccDigits.At(4) == 5038 || ccDigits.At(4) == 5612 || ccDigits.At(4) == 5893 || ccDigits.At(4) == 6304 || ccDigits.At(4) == 6759 || ccDigits.At(4) == 6761 || ccDigits.At(4) == 6762 || ccDigits.At(4) == 6763 || c.Number[:3] == "0604" || ccDigits.At(4) == 6390:
+	case ccDigits.At(4) == 5018 || ccDigits.At(4) == 5020 || ccDigits.At(4) == 5038 ||
+		ccDigits.At(4) == 5612 || ccDigits.At(4) == 5893 || ccDigits.At(4) == 6304 ||
+		ccDigits.At(4) == 6759 || ccDigits.At(4) == 6761 || ccDigits.At(4) == 6762 ||
+		ccDigits.At(4) == 6763 || c.Number[:3] == "0604" || ccDigits.At(4) == 6390:
 		return Company{"maestro", "Maestro"}, nil
 	case ccDigits.At(4) == 5019:
 		return Company{"dankort", "Dankort"}, nil
 	case ccDigits.At(2) >= 51 && ccDigits.At(2) <= 55:
 		return Company{"mastercard", "MasterCard"}, nil
-	case ccDigits.At(4) == 4026 || ccDigits.At(6) == 417500 || ccDigits.At(4) == 4405 || ccDigits.At(4) == 4508 || ccDigits.At(4) == 4844 || ccDigits.At(4) == 4913 || ccDigits.At(4) == 4917:
+	case ccDigits.At(4) == 4026 || ccDigits.At(6) == 417500 || ccDigits.At(4) == 4405 ||
+		ccDigits.At(4) == 4508 || ccDigits.At(4) == 4844 || ccDigits.At(4) == 4913 || ccDigits.At(4) == 4917:
 		return Company{"visa electron", "Visa Electron"}, nil
 	case ccDigits.At(1) == 4:
 		return Company{"visa", "Visa"}, nil
